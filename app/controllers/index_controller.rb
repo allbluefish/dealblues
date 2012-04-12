8996e40ebdd1d476da7701e0d5076ed3e416b1cc
blue <blue0309@gmail.com>
2012-04-12 17:15:00 +0800

    update controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,9 @@
 class IndexController < ApplicationController
   layout "bootstrap"
   def index
-    @deals = Deal.paginate(:page => params[:page], :per_page => 5).order("pubDate desc").all
+    @deals = Deal.paginate(:page => params[:page], :per_page => 10).order("pubDate desc").all
     @home = "active"
-    p @deals.size
+    @categories = Category.order("count desc").limit(10).all
+    @stores = Store.order("id desc").limit(10).all
   end
 end
