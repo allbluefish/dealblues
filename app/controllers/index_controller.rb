8146d58462be77f9078030aca1bfc4f6d61b577b
blue <blue0309@gmail.com>
2012-04-13 00:04:35 +0800

    add a controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,8 +2,9 @@ class IndexController < ApplicationController
   layout "bootstrap"
   def index
     @deals = Deal.paginate(:page => params[:page], :per_page => 10).order("pubDate desc").all
-    @home = "active"
     @categories = Category.order("count desc").limit(10).all
     @stores = Store.order("id desc").limit(10).all
+
+    @home = "active"
   end
 end
