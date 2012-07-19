44fcb271f4fbeb872091af581e1ac942bb6c7e14
blue <blue0309@gmail.com>
2012-07-19 17:02:43 +0800

    update view title

--- config/initializers/spider_scheduler.rb
+++ config/initializers/spider_scheduler.rb
@@ -7,7 +7,7 @@ class Scheduler
   scheduler = Rufus::Scheduler.start_new
 
 
-  scheduler.every '3h' do
+  scheduler.every '10m' do
     $application.each { |key, value|
       p "key : #{key},value : #{value}"
       deal = Deal.find(key)
@@ -17,19 +17,19 @@ class Scheduler
     $application = {}
   end
 
+#spider = Spider.new
+
+spiders = Spider.all
+scheduler.every '1h' do
+  spiders.each { |s|
+    p "the site #{s.site} spider date at #{Time.now.strftime("%Y-%m-%d %H:%M")}"
+    p s.clutch
+}
+end
 
 #scheduler.every '20m' do
 #  respond_to "order ristretto"
 #end
 
-#spider = Spider.new
-
-#spiders = Spider.all
-#scheduler.every '5s' do
-#  spiders.each { |s|
-#    p "the site #{s.site} spider date at #{Time.now.strftime("%Y-%m-%d %H:%M")}"
-#    p s.site
-#}
-#end
 
 end
