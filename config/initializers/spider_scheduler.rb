2224eb9782f48f3fd96aadc6667dde5122e78d23
blue <blue0309@gmail.com>
2012-07-28 16:40:56 +0800

    update views

--- config/initializers/spider_scheduler.rb
+++ config/initializers/spider_scheduler.rb
@@ -19,14 +19,32 @@ class Scheduler
 
 #spider = Spider.new
 
-scheduler.every '3h' do
-  spiders = Spider.all
-  spiders.each { |s|
-    p "the site #{s.site} spider date at #{Time.now.strftime("%Y-%m-%d %H:%M")}"
-    s.clutch
-}
-end
+  scheduler.every '3h' do
+    spiders = Spider.all
+    spiders.each { |s|
+      p "the site #{s.site} spider date at #{Time.now.strftime("%Y-%m-%d %H:%M")}"
+      s.clutch
+    }
+  end
 
+  month = 60 * 60 * 24 * 30
+  scheduler.every '2m' do
+    now = Time.now
+    before = (now + month).strftime('%Y-%m-%d')
+    deals = Deal.where("pubDate < '#{before}'").order("pubDate desc").all
+    deals.each { |d|
+      d.categories.each { |c|
+        c_count = c.count
+        c.update_attribute('count', c_count - 1)
+      }
+
+      store = d.store
+      s_count = store.count
+      store.update_attribute('count', s_count - 1)
+    }
+
+    Deal.delete_all("pubDate < '#{before}'")
+  end
 #scheduler.every '20m' do
 #  respond_to "order ristretto"
 #end
